000e453b6b382fcdb2142e5046245b42e7db31b5
Nicolas Ardison <nicolas.ard@gmail.com>
2021-05-09 12:36:42 -0300

    Example working user/password: user/user

--- example-1/service-server-definition/src/main/java/ar/nic/example/not/ExampleApplication.java
+++ example-1/service-server-definition/src/main/java/ar/nic/example/not/ExampleApplication.java
@@ -1,4 +1,4 @@
-package ar.nic.example;
+package ar.nic.example.not;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ example-1/service-server-definition/src/main/java/ar/nic/example/not/controllers/PetsController.java
@@ -0,0 +1,47 @@
+package ar.nic.example.not.controllers;
+
+import ar.nic.example.openapi.ApiUtil;
+import ar.nic.example.openapi.PetsApi;
+import ar.nic.example.openapi.model.Pet;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("${openapi.swaggerPetstore.base-path:/}")
+public class PetsController implements PetsApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public PetsController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+    @Override
+    public ResponseEntity<List<Pet>> listPets(@Valid Integer limit) {
+        getRequest().ifPresent(request -> {
+            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"name\" : \"name\", \"id\" : 103, \"tag\" : \"tag\" }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+        });
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
